2e5cdcbeed0121d4b6fd42bb1b0806be59e30df3
mayankpcp7 <jariwalmayank123@gmail.com>
2024-06-09 13:20:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="overflow-hidden">
+      <p className="text-5xl text-black text-center font-plusJkarta font-semibold">hello</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        darkYellow: "#E0A922",
+        gold: "#EDC743",
+        darkGold: "#D6AA14",
+        lightBrown: "#FCF7E9",
+        offBlack: "#0C0C0C",
+        offGrey: "#666666",
+        grey: "#525252",
+        lightBlack: "#1A1A1A",
+        lightGrey: "#DADADA",
+        thinGrey: "#4D524D",
+        faqPeraBlack: "#3E3E3E",
+        red: "#ff0000",
+        blogPera: "#000700",
+        blogBorder: "#D4D4D4",
+        skyBlue: "#4286F4",
+      },
+      fontFamily: {
+        plusJkarta: "Plus Jakarta Sans",
+        graphik: "Graphik",
+      },
+      fontSize: {
+        "6xl": "64px",
+        "3xl": "28px",
+        "4xl": "42px",
+      },
+    },
+  },
+  plugins: [],
+};
