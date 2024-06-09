d65adc704fd162f37aa7e38121059209c7378e12
khushi303 <maankhushi303@gmail.com>
2024-06-09 15:08:26 +0530

    adding tsatopchoice file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import TsaTopChoice from "./components/TsaTopChoice";
 
 function App() {
   return (
     <div className="overflow-hidden">
       <p className="text-5xl text-black text-center font-plusJkarta font-semibold">hello</p>
+      <TsaTopChoice/>
     </div>
   );
 }
---
+++ src/components/TsaTopChoice.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TsaTopChoice = () => {
+  return (
+      <div>
+          
+    </div>
+  )
+}
+
+export default TsaTopChoice
