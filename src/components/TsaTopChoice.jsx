fe778c5a31fa9c7e6891c72eaf71c8ea075a12b3
khushi303 <maankhushi303@gmail.com>
2024-06-10 10:04:39 +0530

    adding map array for tsa top chioce and installing swiper slider

--- src/components/TsaTopChoice.jsx
+++ src/components/TsaTopChoice.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 
 const TsaTopChoice = () => {
   return (
-      <div>
-          
+    <div className='pt-32 mt-3'>
+      <div className='container xl:max-w-[1172px] px-4 mx-auto'>
+        <h2 className='font-bold sm:text-5xl text-2xl text-black text-center max-w-[563px] mx-auto !leading-md'>TSA is the top choice by families like you.</h2>
+        <div>
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]} className="mySwiper">
+            <SwiperSlide className='border border-'>Slide 1</SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/common/Helper.jsx
@@ -0,0 +1,25 @@
+import brownGirl from '../../assets/images/webp/tsaTopChoice/brownGirl.webp'
+import yellowLady from '../../assets/images/webp/tsaTopChoice/yellowLady.webp'
+import dreamTemple from '../../assets/images/webp/tsaTopChoice/dreamTemple.webp'
+import artistGuy from '../../assets/images/webp/tsaTopChoice/artistGuy.webp'
+import beautifulCollection from '../../assets/images/webp/tsaTopChoice/beautifulCollection.webp'
+
+
+// ===============================TSA TOP CHOICE =============================//
+export const topChoiceCardlist = [
+    {
+        img: brownGirl,
+    },
+    {
+        img: yellowLady,
+    },
+    {
+        img: dreamTemple,
+    },
+    {
+        img: artistGuy,
+    },
+    {
+        img: beautifulCollection,
+    },
+]
