934b8be9293395faf1c1d7016f387c30cb3d4ab7
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2024-06-10 12:36:03 +0530

    this your section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import GetInspired from "./components/GetInspired";
+import ThisYou from "./components/ThisYou";
 
 function App() {
   return (
     <div className="overflow-hidden">
       <GetInspired />
+      <ThisYou/>
     </div>
   );
 }
---
+++ src/components/common/Helper.jsx
@@ -0,0 +1,59 @@
+import dreamEmoji from "../../assets/images/webp/thisYou/dreamEmoji.webp";
+import sleepEmoji from "../../assets/images/webp/thisYou/sleepEmoji.webp";
+import facehideEmoji from "../../assets/images/webp/thisYou/facehideEmoji.webp";
+import handsGroup from "../../assets/images/webp/thisYou/handsGroup.webp";
+import twoPerson from "../../assets/images/webp/thisYou/twoPerson.webp";
+import deliveryBoy from "../../assets/images/webp/thisYou/deliveryBoy.webp";
+import professional from "../../assets/images/webp/thisYou/professional.webp";
+import thumb from "../../assets/images/webp/thisYou/thumb.webp";
+import templeToday from "../../assets/images/webp/thisYou/templeToday.webp";
+
+export const ThisYouData = [
+  {
+    emoji: dreamEmoji,
+    description: "You are looking for someone to make your dream pooja room",
+    customClass: "dreamEmoji",
+  },
+  {
+    emoji: sleepEmoji,
+    description:
+      "You are tired of finding the right brand who can do it for you",
+    customClass: "sleepEmoji",
+  },
+  {
+    emoji: facehideEmoji,
+    description: "Can’t find someone to design pooja room for you",
+    customClass: "hidefaceEmoji"
+  },
+  {
+    emoji: handsGroup,
+    description: "Can’t trust anyone with your most important room",
+    customClass: "handsGroup",
+  },
+  {
+    emoji: twoPerson,
+    description: "Local vendors are not up to the mark",
+    customClass: "twopersonEmoji",
+  },
+  {
+    emoji: deliveryBoy,
+    description: "Need a one stop shop who can design deliver & install",
+    customClass: "deliveryBoy",
+  },
+  {
+    emoji: professional,
+    description: "Need a professionals help",
+    customClass: "professionEmoji",
+  },
+  {
+    emoji: thumb,
+    description:
+      "That's exactly the idea! It succinctly communicates the message",
+    customClass: "thumbEmoji",
+  },
+  {
+    emoji: templeToday,
+    description: "Relate to these? We'll help build your dream temple today.",
+    customClass: "templeEmoji",
+  },
+];
