a6539d358e3fed1622c5fb2eb59886ffe458566e
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-06-10 09:46:17 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import PursuitSec from "./components/PursuitSec";
 
 function App() {
   return (
     <div className="overflow-hidden">
-      <p className="text-5xl text-black text-center font-plusJkarta font-semibold">hello</p>
+      <p className="text-5xl font-semibold text-center text-black font-plusJkarta">hello</p>
+      <PursuitSec />
     </div>
   );
 }
---
+++ src/components/PursuitSec.jsx
@@ -0,0 +1,23 @@
+
+const PursuitSec = () => {
+
+    const RightTick = () => {
+        return (
+            <svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M16.5748 0.0788631C16.4435 0.130698 15.0839 1.34883 12.0975 4.07884C9.7393 6.23866 7.3811 8.39415 6.8607 8.86931L5.91367 9.73324L3.76645 7.76349C2.585 6.67926 1.55358 5.7635 1.47857 5.72894C0.972238 5.49568 0.34401 5.68143 0.0955318 6.13499C-0.0310515 6.35529 -0.0310515 6.76565 0.0908435 6.98596C0.142414 7.07235 1.32855 8.19977 2.73034 9.48702C4.66659 11.2667 5.32764 11.8499 5.48704 11.9147C5.74958 12.027 6.10589 12.027 6.35437 11.9233C6.60754 11.8153 17.7469 1.63825 17.9016 1.37043C17.9953 1.21493 18.0094 1.13285 17.9953 0.877995C17.9813 0.623137 17.9578 0.541063 17.8453 0.394197C17.5828 0.0399866 17.0014 -0.102562 16.5748 0.0788631Z" fill="#E0A922" />
+            </svg>
+        )
+    }
+    const pursuitSecList = [
+        { title: "Desire a pooja room that’s a true reflection of your faith and aesthetics?" },
+    ]
+    return (
+        <div>
+            <div className="container">
+                <h2>In pursuit of the ideal pooja room? Look no further. if you</h2>
+            </div>
+        </div>
+    )
+}
+
+export default PursuitSec
