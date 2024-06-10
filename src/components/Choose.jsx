57b588db9765d4b7cf33f0d855db39457244e646
prince-372 <pmor18314@gmail.com>
2024-06-10 09:44:41 +0530

    take pull

---
+++ src/components/Choose.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Choose = () => {
+  return (
+    <>
+<div className=' max-w-[963px] mx-auto '>
+      <h2 className='font-plusJkarta font-bold text-5xl leading-[60px] text-center'>Why Choose Tilak</h2>
+</div>
+    </>
+  )
+}
+
+export default Choose
