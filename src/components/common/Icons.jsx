79805385f1c8b163a6f003c4027978e2fd84ead5
khushi303 <maankhushi303@gmail.com>
2024-06-10 16:48:29 +0530

    adding class in config

--- src/components/common/Icons.jsx
+++ src/components/common/Icons.jsx
@@ -9,16 +9,16 @@ export const Plus = () => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        opacity="0.2"
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M30.0218 4.77255C76.8768 27.638 76.395 91.2614 64.5226 142.129C53.516 189.287 23.5539 233.982 -24.5812 238.721C-71.8571 243.376 -112.02 206 -127.327 161.019C-139.318 125.78 -108.18 97.0079 -81.7395 70.7525C-47.7888 37.0395 -12.8949 -16.171 30.0218 4.77255Z"
-        fill="#E0A922"
+        d="M9.5 14.5V1.5M16 8H3"
+        stroke="black"
+        stroke-opacity="0.7"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       />
     </svg>
   );
 };
-
 export const PlayIcon = () => {
   return (
     <svg
