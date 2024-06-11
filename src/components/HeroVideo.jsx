ca92317a00c9710d571f9ddd36dc511297b6d22f
Deepak-Maan <jattdevta1008@gmail.com>
2024-06-11 10:05:34 +0530

    modified width of video section

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -15,12 +15,12 @@ const HeroVideo = () => {
     };
 
     return (
-        <div className="bg-videoBackground lg:bg-cover mt-20 md:mt-[188px] lg:mt-[200px] bg-repeat-round mx-2 md:mx-3 lg:mx-auto md:max-h-[362px] lg:min-h-[597px]   lg:max-w-[1376px]">
+        <div className="bg-videoBackground lg:bg-cover mt-20 md:mt-[188px] lg:mt-[200px] bg-repeat-round mx-2 md:mx-3 lg:mx-3 md:max-h-[362px] lg:min-h-[597px]   lg:max-w-[1376px]">
             <div className='container max-w-[1164px] px-3 mx-auto'>
                 <div className="relative z-[21] -translate-y-6 md:-translate-y-[55px] lg:translate-y-[-92px] ">
                     {isPlaying ? (
                         <video
-                            className=" max-w-[268px] min-[400px]:max-w-[80%] md:max-w-[623px] lg:max-w-[1028px] w-full shadow-[0px_3.61px_72.14px_0px_#00000066] md:max-h-[350px] lg:min-h-[578px] rounded-[18px] mx-auto overflow-hidden"
+                            className=" max-w-[268px] min-[400px]:max-w-[80%] md:max-w-[623px] lg:max-w-[920px] xl:max-w-[1028px] w-full shadow-[0px_3.61px_72.14px_0px_#00000066] md:max-h-[350px] lg:min-h-[578px] rounded-[18px] mx-auto overflow-hidden"
                             controls
                             autoPlay
                             onEnded={handleVideoEnded}>
@@ -32,7 +32,7 @@ const HeroVideo = () => {
                             <img
                                 src={videoThumbnail}
                                 alt="Video Thumbnail"
-                                className="max-w-[268px] min-[400px]:max-w-[80%] md:max-w-[623px] lg:max-w-[1028px] w-full shadow-[0px_3.61px_72.14px_0px_#00000066] border md:border-[2.4px] lg:border-[4px] border-white md:max-h-[350px] lg:min-h-[578px] rounded-[18px] mx-auto"
+                                className="max-w-[268px] min-[400px]:max-w-[80%] md:max-w-[623px] lg:max-w-[920px] xl:max-w-[1028px] w-full shadow-[0px_3.61px_72.14px_0px_#00000066] border md:border-[2.4px] lg:border-[4px] border-white md:max-h-[350px] lg:min-h-[578px] rounded-[18px] mx-auto"
                             />
                             <div className="absolute inset-0 flex items-center justify-center">
                                 <PlayIcon />
